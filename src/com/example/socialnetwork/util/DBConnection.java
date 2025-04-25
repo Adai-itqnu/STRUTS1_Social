2b99b2a124636878fffdcb21bc2bbda0b349c24c
Trần Anh Đài <145352010+Adai-itqnu@users.noreply.github.com>
2025-04-25 20:25:06 +0700

    Update DBConnection.java

--- src/com/example/socialnetwork/util/DBConnection.java
+++ src/com/example/socialnetwork/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/social_network";
     private static final String USER = "root";
-    private static final String PASSWORD = "251204";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         try {
